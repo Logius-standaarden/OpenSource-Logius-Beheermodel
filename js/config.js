1cf68d8d05a9766b6127021c8c7310cfe4a9611c
Edwin Wisse <edwin.wisse@logius.nl>
2024-04-04 15:17:53 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var respecConfig = {
   alternateFormats: [ { 
         "label" : "pdf",
-        "uri" : "Logius-Beheermodel.pdf"
+        "uri" : "OSDBK-Beheermodel.pdf"
       } ],
   authors: [ { 
         "mailto" : "bomos@logius.nl",
@@ -16,11 +16,9 @@ var respecConfig = {
   github: "https://github.com/Logius-standaarden/Logius-Beheermodel",
   nl_markdownCodeClass: "code",
   nl_markdownTableClass: "dkkvs",
-  previousPublishDate: "2020-12-31",
-  previousPublishVersion: "0.1",
   pubDomain: "logius",
   publishDate: "2023-09-23",
-  publishVersion: "1.0",
+  publishVersion: "0.1",
   shortName: "beheermodel",
   specStatus: "WV",
   specType: "HR"
